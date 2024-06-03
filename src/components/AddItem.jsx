bd0e7b8d37c4edee7d10d81b1dbeda25580ff73c
nc-michael-rombi <michael.rombi@northcoders.com>
2024-06-03 08:36:19 +0100

    will fix a bug where multiple items were added at once. Will add a quantity to keep track of items with the same id.

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -4,8 +4,24 @@ import { Context } from "../App";
 function AddItem({ setList, item }) {
   const [, setTotal] = useContext(Context);
   function handleClick(item) {
+    let wasInList = false;
+
     setList((currItems) => {
-      return [...currItems, item];
+      const newItem = {
+        item_id: item.item_id,
+        item_name: item.item_name,
+        price: item.price,
+        quantity: 1,
+      };
+
+      currItems.forEach((currItem) => {
+        if (currItem.item_id === item.item_id) {
+          wasInList = true;
+          currItem.quantity++;
+        }
+      });
+      if (wasInList) return [...currItems];
+      return [...currItems, newItem];
     });
     setTotal((currTotal) => currTotal + item.price);
   }
